554591219e46e0f380b475ad6cf9c779d6304fc5
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-21 04:35:35 -0700

    write operate function using switch case
    takes 2 nums and operator, returns result

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const divide = (num1, num2) => num1 / num2;
 let firstDisplayNum;
 let displayOperator;
 let secondDisplayNum;
+
+const operate = (num1, num2, operator) => {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+    }
+};
+
+console.log(operate(2, 2, '+'));
+console.log(operate(7, 2, '-'));
+console.log(operate(3, 5, '*'));
+console.log(operate(21, 7, '/'));
