03ee0a13f19719cc890c4f743d7f9f7ba46d9159
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-21 04:25:20 -0700

    write divide function accepting 2 parameters
    returns division of 2 arguments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
 
-console.log(multiply(4, 2));
-console.log(multiply(5, 2.25));
+console.log(divide(10, 2));
+console.log(divide(10, 3));
